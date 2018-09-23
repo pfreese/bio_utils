9a12fe6adff28cf79a5067d445b4044d31cdea8d
Peter Freese <freese.peter@gmail.com>
2018-09-23 16:30:55 -0700

    parse CSVs and TSVs

--- scripts/get_max_csv_val.go
+++ scripts/get_max_csv_val.go
@@ -7,24 +7,53 @@ import (
 	"log"
 	"os"
 	"strconv"
+
 	"github.com/pkg/errors"
 )
 
-func main() {
-	f, _ := os.Open("../pkg/test_data/test_vals_0.csv")
+type Params struct {
+	csvSep string
+}
+
+func getMaxFromFirstColumn(File string, csvSep rune) {
+	f, _ := os.Open(File)
 	r := csv.NewReader(bufio.NewReader(f))
+	// Set the 'comma' to be csvSep, likely either '\t' or ','
+	r.Comma = csvSep
+
 	records, _ := r.ReadAll()
 
 	var intMax int
 	for _, record := range records {
+		fmt.Println(record)
+		// Convert the string to int
 		intVal, err := strconv.Atoi(record[0])
 		if err != nil {
 			err = errors.Wrap(err, "Parse failed")
 			log.Fatal(err)
 		}
+		// Update if this line contains the greatest value seen so far
 		if intVal > intMax {
 			intMax = intVal
 		}
 	}
 	fmt.Println("Max val = ", intMax)
+}
+
+func getMaxFromCSV(File string) {
+	getMaxFromFirstColumn(File, ',')
+}
+
+func getMaxFromTSV(File string) {
+	getMaxFromFirstColumn(File, '\t')
+}
+
+func main() {
+	// Test a comma-separated file
+	testCSV := "../pkg/test_data/test_vals_0.csv"
+	getMaxFromCSV(testCSV)
+
+	// Test a tab-separated file
+	testTSV := "../pkg/test_data/test_vals_0.tsv"
+	getMaxFromTSV(testTSV)
 }
